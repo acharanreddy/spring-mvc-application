ec4ee9b00c1d9aced9127b460b2d6a5a7bd80ab6
charan <charanreddyanumula@gmail.com>
2019-08-07 14:28:06 +0530

    first commit

---
+++ src/main/java/com/dbs/springmvcapp/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.dbs.springmvcapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/users")
+public class EmployeeController {
+  //Create EmpService OBJ
+    
+    @GetMapping(value = "/login")
+    public String login(Model model){
+        System.out.println("Came inside the login method ");
+
+        return "login";
+    }
+    @PostMapping("/login")
+    public String loginUser(
+    		@RequestParam("username") String username, 
+    		@RequestParam("password") String password,
+    		 Model model){
+        System.out.println("Inside the POST method of login user");
+        System.out.println("Username is"+username+ "password is"+password);
+        if(username.equalsIgnoreCase(password)) {
+        	model.addAttribute("user", username);
+        	return "success";
+        }
+        return "login";
+    }
+    
+    @GetMapping("/listAll")
+    public String listAllEmployee(Model model) {
+    	
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/login.jsp
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+</head>
+<body>
+
+    <h1>Login page !!</h1>
+    <form action="" method="post">
+    UserName:<input type="text" name="username"> 
+    Password:<input type="password" name="password">
+    <input type="submit">
+    <input type="reset">
+    </form>
+</body>
+</html>
